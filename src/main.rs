557281f62237caad20058c70903a83080f28fea1
Ty Overby <ty@pre-alpha.com>
2019-05-26 14:56:25 -0400

    _

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,43 @@
+extern crate flame;
 extern crate osm_xml;
 extern crate proj5;
 
-use osm_xml::{Member, Reference, Tag, Way, OSM};
+#[macro_use]
+extern crate flamer;
+mod osm_load;
+use osm_load::*;
 
-use proj5::FromLonLat;
-use proj5::{crs::MercatorSystem, *};
-
-use std::fs::File;
-use std::io::BufReader;
 use std::ops::Range;
+
 const TARGET_H: f64 = 1000.0f64;
 
-fn filter(tag: &Tag) -> bool {
-    eprintln!("k {} v {}", tag.key, tag.val);
-    eprintln!("key {}", tag.key);
-    eprintln!("val {}", tag.val);
-    match tag.key.as_str() {
-        "building" | "pier" | "lanes" | "highway" | "subway" => return true,
-        _ => (),
-    };
-    match tag.val.as_str() {
-        "pier" | "waterway" | "riverbank" | "water" | "coastline" => true,
-        _ => false,
+enum Kind {
+    Building(Range<usize>),
+    Road(Range<usize>),
+}
+
+fn filter(
+    _relationship_tags: &[Tag],
+    way_tags: &[Tag],
+    range: Range<usize>,
+) -> Option<Kind> {
+    if way_tags.iter().any(|tag| tag.key == "highway") {
+        Some(Kind::Road(range))
+    } else if way_tags.iter().any(|tag| tag.key == "building") {
+        Some(Kind::Building(range))
+    } else {
+        None
     }
 }
-
-fn convert(input: Vec<(f64, f64)>) -> Vec<(f64, f64)> {
-    let ellipsoid = WGS_1984_ELLIPSOID;
-    //let system = UTMSystem { utm_zone: 10 };
-    let system = MercatorSystem;
-
-    let mut strategy = MultithreadingStrategy::MultiCore(ThreadPool::new(8));
-    let out = system.from_lon_lat(input, &ellipsoid, &mut strategy);
-    out.data
-}
-fn print_path<I>(path: I)
+fn print_path<I>(path: I, is_building:bool)
 where
     I: Iterator<Item = (f64, f64)>,
 {
-    print!(r#"<path style="fill:none; stroke:black;" d=""#);
+    if is_building {
+        print!(r#"<path style="fill:none; stroke:red; stroke-width:15px" d=""#);
+    } else {
+        print!(r#"<path style="fill:none; stroke:black; stroke-width:15px" d=""#);
+    }
     let mut first = true;
     for (lon, lat) in path {
         let movement = if first { "M" } else { "L" };
@@ -48,95 +47,54 @@ where
     println!(r#"" />"#);
 }
 
-fn print_way(
-    way: &Way,
-    always_print: bool,
-    all_coords: &mut Vec<(f64, f64)>,
-    all_polys: &mut Vec<Range<usize>>,
-    doc: &OSM,
-) {
-    if !always_print && !way.tags.iter().any(filter) {
-        return;
-    }
-    let start = all_coords.len();
-
-    for node in &way.nodes {
-        let node = doc.resolve_reference(node);
-        if let Reference::Node(node) = node {
-            all_coords.push((node.lon, node.lat));
-        }
-    }
-    let end = all_coords.len();
-    all_polys.push(start..end);
-}
-
 fn main() {
-    // curl 'https://www.openstreetmap.org/api/0.6/map?bbox=-73.9846%2C40.6791%2C-73.9737%2C40.6911' > nyc.osm
-    let f = File::open("./nyc.osm").unwrap();
-    let br = BufReader::new(f);
-    eprintln!("reading");
-    let doc = OSM::parse(br).unwrap();
-    eprintln!(
-        "{} nodes, {} relations, {} ways",
-        doc.nodes.len(),
-        doc.relations.len(),
-        doc.ways.len()
-    );
-    eprintln!("done reading");
-
-    let bounds = doc.bounds.unwrap();
-
-    let bounds_converted = convert(vec![
-        (bounds.maxlon, bounds.maxlat),
-        (bounds.minlon, bounds.minlat),
-    ]);
-    let (b_max_lon, b_max_lat) = bounds_converted[0];
-    let (b_min_lon, b_min_lat) = bounds_converted[1];
-    let target_w = ((b_max_lon - b_min_lon) / (b_max_lat - b_min_lat)) * TARGET_H;
-    let scale_x = target_w / (b_max_lon - b_min_lon);
-    let scale_y = TARGET_H / (b_max_lat - b_min_lat);
+    let Geometry {
+        bounds,
+        coords,
+        polys,
+        results,
+    } = load_osm_file("./nyc.osm", &filter, 1000.0);
+    let osm_load::Bounds {
+        width,
+        height,
+        min_lon,
+        min_lat,
+        scale_x,
+        scale_y,
+        ..
+    } = bounds;
 
     println!(
         r#"<svg viewBox="0 0 {} {} " xmlns="http://www.w3.org/2000/svg">"#,
-        target_w, TARGET_H
+        width, height
     );
 
-    let mut all_coords = Vec::new();
-    let mut all_polys = Vec::new();
+    let transform =
+|&(lon, lat)| ((lon - min_lon) * scale_x, (lat - min_lat) * scale_y) ;
 
-    eprintln!("finding relations");
-
-    for rel in doc.relations.values() {
-        if !rel.tags.iter().any(filter) {
-            continue;
+for kind in results {
+    match kind {
+        Kind::Road(range) => {
+          let scaled = coords[range] .iter().map(transform);
+          print_path(scaled,false)
         }
-        for member in &rel.members {
-            if let &Member::Way(ref reference, _) = member {
-                let member = doc.resolve_reference(reference);
-                if let Reference::Way(way) = member {
-                    print_way(way, true, &mut all_coords, &mut all_polys, &doc);
-                }
-            }
+        Kind::Building(range) => {
+          let scaled = coords[range] .iter().map(transform);
+          print_path(scaled, true)
         }
     }
-    eprintln!("done finding relations");
-    eprintln!("finding ways");
+}
+    /*
 
-    for way in doc.ways.values() {
-        print_way(way, false, &mut all_coords, &mut all_polys, &doc);
-    }
-    eprintln!("done finding ways");
-
-    eprintln!("converting");
-    let all_coords = convert(all_coords);
-    eprintln!("done converting");
-
-    for poly in all_polys {
-        let scaled = all_coords[poly]
+    for poly in polys {
+        let scaled = coords[poly]
             .iter()
-            .map(|&(lon, lat)| ((lon - b_min_lon) * scale_x, (lat - b_min_lat) * scale_y));
+            .map(|&(lon, lat)| ((lon - min_lon) * scale_x, (lat - min_lat) * scale_y));
         print_path(scaled);
     }
+    */
 
     println!("</svg>");
+
+    flame::dump_html(std::fs::File::create("./flame.html").unwrap()).unwrap();
 }
---
+++ src/osm_load.rs
@@ -0,0 +1,147 @@
+use osm_xml::{Member, Reference, Way, OSM};
+
+use proj5::FromLonLat;
+use proj5::{crs::MercatorSystem, *};
+
+use std::fs::File;
+use std::io::BufReader;
+use std::ops::Range;
+
+pub use osm_xml::Tag;
+
+pub type Callback<'a, T> = &'a Fn(&[Tag], &[Tag], Range<usize>) -> Option<T>;
+
+pub struct Geometry<T> {
+    pub bounds: Bounds,
+    pub results: Vec<T>,
+    pub coords: Vec<(f64, f64)>,
+    pub polys: Vec<Range<usize>>,
+}
+pub struct Bounds {
+    pub width: f64,
+    pub height: f64,
+    pub min_lon: f64,
+    pub min_lat: f64,
+    pub max_lon: f64,
+    pub max_lat: f64,
+    pub scale_x: f64,
+    pub scale_y: f64,
+}
+
+#[flame]
+fn collect_ways<T>(
+    relationship_tags: Option<&[Tag]>,
+    way: &Way,
+    callback: Callback<T>,
+    all_coords: &mut Vec<(f64, f64)>,
+    all_polys: &mut Vec<Range<usize>>,
+    all_values: &mut Vec<T>,
+    doc: &OSM,
+) {
+    let tags = &way.tags;
+    let start = all_coords.len();
+    let end = start + way.nodes.len();
+
+    let relationship_tags = relationship_tags.unwrap_or(&[]);
+
+    if let Some(v) = callback(relationship_tags, tags, start..end) {
+        all_values.push(v);
+        for node in &way.nodes {
+            let node = doc.resolve_reference(node);
+            if let Reference::Node(node) = node {
+                all_coords.push((node.lon, node.lat));
+            }
+        }
+        let end = all_coords.len();
+        all_polys.push(start..end);
+    }
+}
+
+#[flame]
+fn coord_convert(input: Vec<(f64, f64)>) -> Vec<(f64, f64)> {
+    let ellipsoid = WGS_1984_ELLIPSOID;
+    //let system = UTMSystem { utm_zone: 10 };
+    let system = MercatorSystem;
+
+    let mut strategy = MultithreadingStrategy::MultiCore(ThreadPool::new(8));
+    let out = system.from_lon_lat(input, &ellipsoid, &mut strategy);
+    out.data
+}
+
+#[flame]
+pub fn load_osm_file<T>(path: &str, callback: Callback<T>, target_h: f64) -> Geometry<T> {
+    // curl 'https://www.openstreetmap.org/api/0.6/map?bbox=-73.9846%2C40.6791%2C-73.9737%2C40.6911' > nyc.osm
+    let f = File::open(path).unwrap();
+    let br = BufReader::new(f);
+    let doc = flame::span_of("reading osm data", || OSM::parse(br).unwrap());
+    let bounds = doc.bounds.unwrap();
+
+    let bounds_converted = coord_convert(vec![
+        (bounds.maxlon, bounds.maxlat),
+        (bounds.minlon, bounds.minlat),
+    ]);
+    let (b_max_lon, b_max_lat) = bounds_converted[0];
+    let (b_min_lon, b_min_lat) = bounds_converted[1];
+    let target_w = ((b_max_lon - b_min_lon) / (b_max_lat - b_min_lat)) * target_h;
+    let scale_x = target_w / (b_max_lon - b_min_lon);
+    let scale_y = target_h / (b_max_lat - b_min_lat);
+
+    let mut all_coords = Vec::new();
+    let mut all_polys = Vec::new();
+    let mut all_values = Vec::new();
+
+    flame::span_of("finding relations", || {
+        for rel in doc.relations.values() {
+            let rel_tags = &rel.tags;
+            for member in &rel.members {
+                if let &Member::Way(ref reference, _) = member {
+                    let member = doc.resolve_reference(reference);
+                    if let Reference::Way(way) = member {
+                        collect_ways(
+                            Some(rel_tags),
+                            way,
+                            callback,
+                            &mut all_coords,
+                            &mut all_polys,
+                            &mut all_values,
+                            &doc,
+                        );
+                    }
+                }
+            }
+        }
+
+    });
+
+    flame::span_of("finding ways", || {
+        for way in doc.ways.values() {
+            collect_ways(
+                None,
+                way,
+                callback,
+                &mut all_coords,
+                &mut all_polys,
+                &mut all_values,
+                &doc,
+            );
+        }
+    });
+
+    let all_coords = coord_convert(all_coords);
+
+    Geometry {
+        bounds: Bounds {
+            min_lon: b_min_lon,
+            min_lat: b_min_lat,
+            width: target_w,
+            height: target_h,
+            max_lon: b_max_lon,
+            max_lat: b_max_lat,
+            scale_x,
+            scale_y,
+        },
+        coords: all_coords,
+        polys: all_polys,
+        results: all_values,
+    }
+}
